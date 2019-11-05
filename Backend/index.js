de531613ddaf4f798e0265c773ad7b54fcd7d738
Ty <txtrinh@iastate.edu>
2019-11-04 20:06:22 -0600

    finish api register and users

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 const app = express();
 
 app.use(bodyParser.json());
@@ -12,4 +13,62 @@ app.get('/',function(req, res)
 app.listen(3000, function()
 {
     console.log('Running on port 3000');
+});
+
+const sequelize = new Sequelize
+({
+    database: 'remnisense_db',
+    username: 'root',
+    password: 'password',
+    dialect: 'mysql',
+});
+
+sequelize.authenticate().then(() => console.log('Connection established')).catch(err => console.error('unable to connect to database', err));
+
+const User = sequelize.define('user', 
+{
+    name: 
+    {
+        type: Sequelize.STRING,
+    },
+    password:
+    {
+        type: Sequelize.STRING,
+    },
+});
+
+User.sync()
+.then(() => console.log('User Table Created Successfully'))
+.catch(err => console.log ('Unable to create the user table'));
+
+const createUser = async ({name, password}) => 
+{
+    return await User.create({name, password});
+}
+
+const getAllUsers = async () =>
+{
+    return await User.findAll();
+};
+
+const getUser = async obj =>
+{
+    return await User.findOne
+    (
+        {
+            where: obj,
+        }
+    );
+};
+
+app.get('/users', function(req, res)
+{
+    getAllUsers().then(user => res.json(user));
+});
+
+app.post('/register', function(req, res, next)
+{
+    const{name, password} = req.body;
+    createUser({name,password}).then(user =>
+        res.json({user, msg: 'account created'}));
 });
