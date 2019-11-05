77593092b3f50e168f15174266361828ab8b6e25
Talen Fisher <talen.fisher@cythral.com>
2019-11-05 09:33:04 -0600

    do a basic deploy

---
+++ deploy/create-config-files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+image=$1
+cwd=$(dirname "${BASH_SOURCE[0]}")
+
+for file in $(ls ${cwd}/params/*.json); do
+    envName=$(echo $file | xargs basename | sed "s/\.json//")
+    params=$(cat $file)
+    params=$(echo $params | jq ".Image=\"$image\"")
+
+    config={}
+    config=$(echo $config | jq --argjson params "$params" '.Parameters=$params')
+    echo $config > ${envName}.config.json
+done
