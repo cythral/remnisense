f19480a1f6af7c86716f51fa0b6e67106f3e1a00
Talen Fisher <talen.fisher@cythral.com>
2019-11-07 14:26:00 -0600

    add client build instructions

--- deploy/create-config-files.sh
+++ deploy/create-config-files.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 apiImage=$1
+clientImage=$2
 cwd=$(dirname "${BASH_SOURCE[0]}")
 
 for file in $(ls ${cwd}/params/*.json); do
     envName=$(echo $file | xargs basename | sed "s/\.json//")
     params=$(cat $file)
     params=$(echo $params | jq ".ApiImage=\"$apiImage\"")
-
+    params=$(echo $params | jq ".ClientImage=\"$clientImage\"")
+    
     config={}
     config=$(echo $config | jq --argjson params "$params" '.Parameters=$params')
     echo $config > ${envName}.config.json
