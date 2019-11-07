119756c04de927153d68d4d145b1ad27208b4893
Talen Fisher <talen.fisher@cythral.com>
2019-11-07 13:47:34 -0600

    splitting out api cicd into its own thing

--- deploy/create-config-files.sh
+++ deploy/create-config-files.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-image=$1
+apiImage=$1
 cwd=$(dirname "${BASH_SOURCE[0]}")
 
 for file in $(ls ${cwd}/params/*.json); do
     envName=$(echo $file | xargs basename | sed "s/\.json//")
     params=$(cat $file)
-    params=$(echo $params | jq ".Image=\"$image\"")
+    params=$(echo $params | jq ".ApiImage=\"$apiImage\"")
 
     config={}
     config=$(echo $config | jq --argjson params "$params" '.Parameters=$params')
