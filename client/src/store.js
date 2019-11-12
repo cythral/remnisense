62c683a9c1e8c80462b7b0c684e31c74dddc51d8
Talen Fisher <talen.fisher@cythral.com>
2019-11-12 15:48:15 -0600

    add persistent state for the client

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersist from "vuex-persist";
 
+// required when bundled
 window.Vue = Vue;
 
 export default new Vuex.Store({
-
+    plugins: [
+        new VuexPersist({
+            key: 'remnisense-store',
+            storage: localStorage
+        })
+    ]
 });
